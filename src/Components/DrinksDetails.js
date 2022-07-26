6a365db75f74b4765c16109fc6a8278c7511a19d
borebrian2021 <bkimutai2021@gmail.com>
2022-07-26 22:07:34 +0300

    Completed game details page

--- src/Components/DrinksDetails.js
+++ src/Components/DrinksDetails.js
@@ -11,9 +11,13 @@ function DrinksDetails(){
 
                   <motion.img initial={{x:1500}} animate={{x:-220}} transition={{duration:1.2, delay:1.3, stiffness:200,type: "spring"}} src="https://images.punkapi.com/v2/4.png" class="spring2"/>
  */}
+ 
 
 <motion.img initial={{x:1500}} animate={{x:0}} transition={{duration:0.8, delay:1.4, stiffness:200,type: "spring"}} src="https://purepng.com/public/uploads/large/purepng.com-alcohol-bottlebottle-food-wine-object-alcohol-beverage-cocktail-liquor-whiskey-drunk-941524624582wlel2.png" className="logoBig"/>
-<motion.div className="labelTitle " initial={{x:1500}} animate={{x:0}} transition={{duration:0.8, delay:1.6, stiffness:200,type: "spring"}} ><p>Lanson Noble Cuvee</p></motion.div>
+
+
+<motion.div className="labelTitle " initial={{x:1500}} animate={{x:0}} transition={{duration:0.8, delay:1.6, stiffness:200,type: "spring"}} ><h3>Lanson Noble Cuvee | 40% | Scotland</h3><br></br><h2 className='text-white'>Ksh. 10,000.00</h2></motion.div>
+
 <motion.p className="p_white" initial={{x:1500}} animate={{x:0}} transition={{duration:0.8, delay:1.8, stiffness:200,type: "spring"}} >This product is currently in stock and you can place your order now. Through our efficient alcohol delivery system, affordable pricing, and large product collection, we have been rated the best online liquor store in Nairobi. Ordering alcohol online has never been easier; you have two options; do a pre-order or do the regular order. You will receive your products in about 30 minutes. Purchase from one bottle, or cases at wholesale prices.</motion.p>
 <motion.Button className="getStarted" initial={{x:1500}} animate={{x:0}} transition={{duration:0.8, delay:2, stiffness:200,type: "spring"}}>Get Started</motion.Button>
 

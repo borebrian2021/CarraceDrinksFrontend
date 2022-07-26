1ab8b89b9d3cee8558147ca917fd39568c0b2654
borebrian2021 <bkimutai2021@gmail.com>
2022-07-26 17:08:24 +0300

    Added the list

--- src/Components/Drawer.js
+++ src/Components/Drawer.js
@@ -1,11 +1,21 @@
 import react from 'react'
-
+import {motion} from 'framer-motion'
 function Drawer() {
 
     return (
         <div className='drawer'>
+            <motion.img   class="logo" src="https://www.pngall.com/wp-content/uploads/8/Magnifying-Glass-Search-PNG-Transparent-HD-Photo.png" />
+            <input class="search" placeholder="Search drinks.." />
+            <div className="itemParent">
+            <div className="itemList">
 
-<input class="search" placeholder="Search drinks.." />
+            <div className="imageSide">
+                <img class="avatar" src="https://www.pngitem.com/pimgs/m/275-2750746_jameson-irish-whiskey-hd-png-download.png" />
+            </div>
+            <div className="nameSide">
+               <h5 class="text-white">Vodka</h5>
+            </div>
+            </div></div>
         </div>
     )
 }

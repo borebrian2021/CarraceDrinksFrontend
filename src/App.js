a3552c0d66bb6727834aaa785ea6d09659efd2ae
borebrian2021 <bkimutai2021@gmail.com>
2022-07-27 13:50:26 +0300

    Adding router animation

--- src/App.js
+++ src/App.js
@@ -4,27 +4,45 @@ import './font-awesome/css/font-awesome.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import NavBar from './Components/NavBar'
 import Footer from './Components/Footer'
-
+import LogIn from './Components/LogIn'
+import {AnimatePresence} from 'framer-motion'
 import DrinkBody from './Components/DrinkBody'
 import DrinksDetails from './Components/DrinksDetails'
-import LogIn from './Components/LogIn'
-
 import UploadForm from './Components/UploadForm'
-
 import Drawer from './Components/Drawer'
+import {
+  BrowserRouter as Router,  Routes,  Route,useLocation
+}
+from "react-router-dom";
 // import {motion} from 'framer-motion'
 function App() {
+  const location=useLocation();
   return (
-    <div >
-      <NavBar />
-      {/* <Drawer/> */}
-      <LogIn/>
+   
+      <Router location={location} key={location.key}>
+      <NavBar/>
+      <AnimatePresence>
+      <Routes >
+      <Route exact  path='/' element={<DrinkBody/>}>
+      </Route> 
+     
+      <Route exact  path='/DrinksDetails' element={<DrinksDetails/>}>
+      </Route> 
+      <Route exact  path='/LogIn' element={<LogIn/>}>
+      </Route> 
+      <Route exact  path='/UploadForm' element={<UploadForm/>}>
+      </Route> 
+      </Routes>
+      </AnimatePresence>
+      <Footer/>
+      </Router>
     
-      {/* <DrinksDetails /> */}
-      {/* <DrinkBody /> */}
-      {/* <UploadForm/> */}
-      {/* <Footer /> */}
-    </div>
+   
+      
+     
+
+  
+
   );
 }
 

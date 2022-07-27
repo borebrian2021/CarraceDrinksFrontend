1772bde77d4a6f796172372387ad0b7894ce7b1b
borebrian2021 <bkimutai2021@gmail.com>
2022-07-28 00:36:38 +0300

    Done with form validation

--- src/Components/UploadForm.js
+++ src/Components/UploadForm.js
@@ -4,24 +4,6 @@ import react, { useState } from 'react'
 
 
 import { motion } from 'framer-motion'
-const [category,setContent]=useState("")
-const [name,setName]=useState("")
-const [capacity,setCapacity]=useState("")
-const [image,setImage]=useState("")
-const [drinkPoster,setdrinkPoster]=useState("")
-const [smallDrink,setsmallDrink]=useState("")
-const [price,setPrice]=useState("")
-const [percentage,setPercentage]=useState("")
-const [description,setDescription]=useState("")
-const [cog,setCog]=useState("")
-const [brand,setBrand]=useState("")
-
-function handleSetCategory(e){
-    setContent(e.target.value)
-}
-
-
-
 
 
 
@@ -47,9 +29,61 @@ const variants_ = {
     }
 }
 function UploadForm() {
+const [category,setCategory]=useState()
+const [name,setName]=useState()
+const [capacity,setCapacity]=useState()
+const [image,setImage]=useState()
+const [price,setPrice]=useState()
+const [percentage,setPercentage]=useState()
+const [description,setDescription]=useState()
+const [cog,setCog]=useState()
+
+function handleSetCategory(event){
+    setCategory(event.target.value)
+}
+function handleSetName(e){
+    setName(e.target.value)
+}
+function handleSetCapacity(e){
+    setCapacity(e.target.value)
+}
+
+function handleSetPrice(e){
+    setPrice(e.target.value)
+}
+
+function handleSetPercentage(e){
+    setPercentage(e.target.value)
+}
+function handleSetDescription(e){
+    setDescription(e.target.value)
+}
+function handleSetCog(e){
+    setCog(e.target.value)
+}
+
+function handleSetImage(e){
+    setImage(e.target.value)
+}
+function handleSubmit(event){
+event.preventDefault()
+const toSubmit={
+    category:category,
+    name:name,
+    capacity:capacity,
+    imageSrc:image,
+    price:price,
+    percentage:percentage,
+    description:description,
+    CountryOfOrigin:cog,
+
+}
+console.log(toSubmit)
+}
+
     return (
         <motion.div variants={variants_} initial="hidden" exit="exit" animate="visible" className="ddrinksDetails pt-4 ">
-            <form class="row bg_dark2">
+            <form class="row bg_dark2" onSubmit={handleSubmit}>
                 <div class="col-12 col-lg-6 col-md-6 col-sm-12 p-5 center_first_flex">
                     <motion.img initial={{ scale:0 }} animate={{ scale:1}} transition={{ duration: 4, delay: 0.2, stiffness: 120, type: "spring" }} src="./images/bottles.png" className='bottles' />
                     <h6 class="text-white">Brian Kimutai Koskei</h6>
@@ -57,16 +91,16 @@ function UploadForm() {
                     <h6 class="text-white">Bevarage upload form.</h6>
                 </div>
                 <div class="col-12 col-lg-6 col-md-6 col-sm-12 p-5  text-left">
-
-
                     <h4 class="text-white">Upload bevarage</h4>
+                   
                     <motion.label initial={{ x: "100vw" }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.2, stiffness: 120, type: "spring" }} class="text-white">Brand Name:</motion.label>
-                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.3, stiffness: 120, type: "spring" }} placeholder='Enter Brand name...' class="form-control m-2" />
-                    <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.4, stiffness: 120, type: "spring" }} class="text-white">Capacitx:</motion.label>
-                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.5, stiffness: 120, type: "spring" }} placeholder='Enter capacity...' class="form-control m-2" />
-                    <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.6, stiffness: 120, type: "spring" }} class="text-white">Categorx:</motion.label>
-
-                    <motion.select initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.7, stiffness: 120, type: "spring" }} className="form-control m-2">
+                    <motion.input value={name} initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.3, stiffness: 120, type: "spring" }} onChange={handleSetName} type="text" placeholder='Enter Brand name...' class="form-control m-2" />
+                    <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.4, stiffness: 120, type: "spring" }} class="text-white">Capacity:</motion.label>
+                    <motion.input initial={{ x: -1500 }} value={capacity} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.5, stiffness: 120, type: "spring" }} onChange={handleSetCapacity} type="text" placeholder='Enter capacity...' class="form-control m-2" />
+                    <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.6, stiffness: 120, type: "spring" }} class="text-white">Category:</motion.label>
+                    <motion.select initial={{ x: -1500 }}  onChange={handleSetCategory} value={category} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.7, stiffness: 120, type: "spring" }}  className="form-control m-2">
+                    <option value=" Not Selected" >--Selected Category--</option>
+                      
                         <option value="Wine" >Wine</option>
                         <option value="Spirit" >Spirit</option>
                         <option value="Gin" >Gin</option>
@@ -74,25 +108,16 @@ function UploadForm() {
                         <option value="rum" >rum</option>
                         <option value="Tequila" >Tequila</option>
                     </motion.select>
-
                     <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.8, stiffness: 120, type: "spring" }} class="text-white">Price:</motion.label>
-                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.9, stiffness: 120, type: "spring" }} placeholder='Enter price...' class="form-control m-2" />
-
+                    <motion.input value={price} initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 0.9, stiffness: 120, type: "spring" }} placeholder='Enter price...'  class="form-control m-2" onChange={handleSetPrice} />
                     <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.0, stiffness: 120, type: "spring" }} class="text-white">Alcohol Percentage:</motion.label>
-                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.1, stiffness: 120, type: "spring" }} placeholder='Enter alcohol percentage...' class="form-control m-2" />
-
+                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.1, stiffness: 120, type: "spring" }} value={percentage} placeholder='Enter alcohol percentage...' onChange={handleSetPercentage} class="form-control m-2" />
                     <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.2, stiffness: 120, type: "spring" }} class="text-white">Description:</motion.label>
-                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.3, stiffness: 120, type: "spring" }} placeholder='Enter description...' class="form-control m-2" />
-
-
-                    <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.4, stiffness: 120, type: "spring" }} class="text-white">Brand:</motion.label>
-                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.5, stiffness: 120, type: "spring" }} placeholder='Enter brand...' class="form-control m-2" />
-
-
-
-
-
-
+                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.3, stiffness: 120, type: "spring" }} value={description} placeholder='Enter description...' onChange={handleSetDescription} class="form-control m-2" />
+                    <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.4, stiffness: 120, type: "spring" }} class="text-white">Image URL:</motion.label>
+                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.5, stiffness: 120, type: "spring" }} value={image} placeholder='Enter image URL...' onChange={handleSetImage} class="form-control m-2" />
+                    <motion.label initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.4, stiffness: 120, type: "spring" }} class="text-white">Country Of Origin:</motion.label>
+                    <motion.input initial={{ x: -1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.5, stiffness: 120, type: "spring" }} value={cog} placeholder='Enter country of origin...' onChange={handleSetCog} class="form-control m-2" />
                     <motion.Button className="getStarted" initial={{ x: 1500 }} animate={{ x: 0 }} transition={{ duration: 0.3, delay: 1.6, stiffness: 120, type: "spring" }} type="submit">Submit</motion.Button>
                 </div>
             </form>
